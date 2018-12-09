5577a2dcbafa0f90a522c80909a49e5eb968ee37
Martin More Panta <martinmore52@gmail.com>
2018-12-08 20:30:53 -0500

    Initial Commit

---
+++ SMSOTP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SMSOTP
+//
+//  Created by Martin More on 12/8/18.
+//  Copyright © 2018 Martin More. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
